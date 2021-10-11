9aa2a8631bf3bd00a29c4c38ee26ef44c6b9ddea
Sugarbell <anonesayu@gmail.com>
2021-10-11 01:26:47 -0600

    v0.51

--- SauceBOT/plugins/sauce_api.py
+++ SauceBOT/plugins/sauce_api.py
@@ -15,15 +15,26 @@ async def __api__(bot, update):
     SAUCE_API = re.sub(r"[^a-zA-Z0-9]", "", text)
     c = await confirm(u, {"user_id": user_id})
     if c:
-        await update_(u,
-                      {"user_id": user_id},
-                      {"SAUCE_API": SAUCE_API})
-        await bot.send_message(chat_id,
-                               "Tu API se ha actualizado con éxito.")
+        if len(text) < 5:
+            await bot.send_message(chat_id,
+                                   "Tu API es:\n"
+                                   "`{}`".format(c[0]["SAUCE_API"]))
+        else:
+            await update_(u,
+                          {"user_id": user_id},
+                          {"SAUCE_API": SAUCE_API})
+            await bot.send_message(chat_id,
+                                   "Tu API se ha actualizado con éxito.")
     else:
-        await add_(u,
-                   {"user_id": user_id,
-                    "SAUCE_API": SAUCE_API})
-        await bot.send_message(chat_id,
-                               "Se ha agregado tu API.")
+        if len(text) < 5:
+            await bot.send_message(chat_id,
+                                   "Tienes que ingresar tu API."
+                                   "Ingresa [SauceNAO](https://saucenao.com/user.php?page=search-api) "
+                                   "y consiguela.")
+        else:
+            await add_(u,
+                       {"user_id": user_id,
+                        "SAUCE_API": SAUCE_API})
+            await bot.send_message(chat_id,
+                                   "Se ha agregado tu API.")
 
