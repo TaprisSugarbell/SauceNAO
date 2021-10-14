9441b0844c4e59e6b472cc051a7787c441019545
Sugarbell <anonesayu@gmail.com>
2021-10-13 20:17:00 -0600

    v0.64

--- SauceBOT/plugins/sauce.py
+++ SauceBOT/plugins/sauce.py
@@ -61,14 +61,14 @@ async def __sauce__(bot, update):
                 try:
                     f = await screenshot(short(google), "".join(dt[3:] + rankey(8) + ".png"))
                 except FileNotFoundError:
-                    os.system("sudo apt-get install wkhtmltopdf -y")
+                    os.system("wget https://github.com/1Danish-00/HtmlWebShot/raw/main/script.sh | sh")
                     sleep(15)
                     f = await screenshot(short(google), "".join(dt[3:] + rankey(8) + ".png"))
             else:
                 try:
                     f = await screenshot(short(google), "".join(dt[3:] + rankey(8) + ".png"))
                 except FileNotFoundError:
-                    os.system("sudo apt-get install wkhtmltopdf -y")
+                    os.system("wget https://github.com/1Danish-00/HtmlWebShot/raw/main/script.sh | sh")
                     sleep(15)
                     f = await screenshot(short(google), "".join(dt[3:] + rankey(8) + ".png"))
             print(f)
