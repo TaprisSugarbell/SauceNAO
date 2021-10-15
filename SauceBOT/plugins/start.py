96d5867905bd4a9adce7d7c7f0058566dff9e48b
Sugarbell <anonesayu@gmail.com>
2021-10-14 20:56:59 -0600

    v0.85

---
+++ SauceBOT/plugins/start.py
@@ -0,0 +1,28 @@
+from pyrogram import Client, filters
+
+
+@Client.on_message(filters.command(["start"]))
+async def __start__(bot, update):
+    print(update)
+    chat_id = update.chat.id
+    await bot.send_message(chat_id,
+                           "Hola, soy **SalsaNAOBot**\n"
+                           "el mejor Reverse Image Search de Telegram, si deseas"
+                           "buscar una imagen en SauceNAO debes ingresar tu "
+                           "[api_key](https://saucenao.com/user.php?page=search-api) "
+                           "si no te has registrado en el sitio debes hacerlo pero el bot"
+                           "funcionara solo que sin darte información buscando en sitios como:\n"
+                           "Google, IQDB, Yandex.\n"
+                           "Para más información puedes ver el comando /help o @SauceNAO")
+
+
+@Client.on_message(filters.command(["help"]))
+async def __help__(bot, update):
+    print(update)
+    chat_id = update.chat.id
+    await bot.send_message(chat_id,
+                           "Info"
+                           "[ ](https://telegra.ph/Info-10-15-2)")
+
+
+
